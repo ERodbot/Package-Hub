e4a900de465a30cbf7108db0a50245f110999310
ERodbot <emarodriguez@estudainec.cr>
2023-11-01 16:07:36 -0600

    cambios de organizacion de assets

--- my-vue-app/src/Pages/User/Main/MainPage.jsx
+++ my-vue-app/src/Pages/User/Main/MainPage.jsx
@@ -10,9 +10,37 @@ import {
 import { useState, useEffect } from "react";
 
 /*Custom css*/
-import "./Navbar.css";
+import "./MainPage.css";
+import PaginaBase from "../../General/PaginaBase/PaginaBase";
+
+/*Imagenes*/
+import snaksmenu from "../../../assets/";
+clothesmenu;
+skatemenu;
+drinksmenu;
+
+const items = [];
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <PaginaBase>
+      <Container>
+        <h1 className="text-center">Títulos Principales</h1>
+        <Row>
+          {["snaks", "bebidad", "ropa", "skate"].map((item, index) => (
+            <Col key={index} xs={6} sm={3}>
+              <Card>
+                <Card.Img src={item.imageSrc} alt={item} />
+                <Card.Body>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>{item.subtitle}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </PaginaBase>
+  );
 };
 export default MainPage;
