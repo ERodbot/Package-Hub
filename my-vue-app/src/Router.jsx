fd94476a896b0cceb5bc985d4833e249bd54cb57
David Fernandez <josfernandez@estudiantec.cr>
2023-11-01 16:19:12 -0600

    Avance Login y Sign

--- my-vue-app/src/Router.jsx
+++ my-vue-app/src/Router.jsx
@@ -1,16 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import NavbarPage from "./Compoments/Navbar/Navbar.jsx";
+import LoginC from "./Pages/User/LoginSignUpC/LoginCliente.jsx";
+import SignC from "./Pages/User/LoginSignUpC/SignUpCliente.jsx";
+
 
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <App />, 
   },
   {
     path: "/navbar",
     element: <NavbarPage />,
   },
+  {
+    path: "/inicioSesionCliente",
+    element: <LoginC />,
+  },
+
+  {
+    path: "/registroCliente",
+    element: <SignC />,
+},
 ]);
 
 export default Router;
