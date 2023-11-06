e97d82d49a815b1e333c3fac15798ac0b13b3f8b
David Fernandez <josfernandez@estudiantec.cr>
2023-11-06 11:46:40 -0600

    arreglos en ordenes y comentarios

--- my-vue-app/src/Pages/Facturacion/OrdenEmpleado.jsx
+++ my-vue-app/src/Pages/Facturacion/OrdenEmpleado.jsx
@@ -3,37 +3,43 @@ import { Container, Row, Col, OverlayTrigger, Popover  } from "react-bootstrap";
 import './Ordenes.css';
 import PaginaBase from "../General/PaginaBase/PaginaBase";
 
+// Elementos que se mostraran en la tabla
 const dataObject = [
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
-  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"] },
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
+  { rowClass: "no-gutters", columns: ["1 of 3", "2 of 3", "3 of 3"], popoverContent: { Etiqueta1: "Valor1", Etiqueta2: "Valor2" }},
 ];
-
-const renderPopover = () => (
+// Funcion que renderiza el contenido del popover
+const renderPopover = (labelsAndValues) => (
   <Popover id="button-popover" className="popOver-content">
-    Contenido del Popover
+    {Object.entries(labelsAndValues).map(([label, value], index) => (
+      <p key={index}>
+        <strong>{label}:</strong> {value}
+      </p>
+    ))}
   </Popover>
 );
 
+// Función que renderiza las filas de la tabla
 const renderRows = (data) => {
   return data.map((row, index) => (
     <OverlayTrigger
       key={index}
       placement="right"
       delay={{ show: 250, hide: 400 }}
-      overlay={renderPopover}
+      overlay={renderPopover({ Etiqueta1: "Valor1", Etiqueta2: "Valor2" })}  // Puedes pasar las etiquetas y valores específicos aquí
       className="overlay-trigger"
     >
       <Row className={index % 2 === 0 ? "even-row" : "odd-row"}>
@@ -47,6 +53,7 @@ const renderRows = (data) => {
   ));
 };
 
+// Función que renderiza la página
 const OrdenesEmpleado = () => (
   <PaginaBase>
     <Container fluid className="mainPage">
